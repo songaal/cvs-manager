08e00db3512bc665954454802f6db5aefeb04c9e
swsong <songaal@gmail.com>
2019-11-03 20:50:58 +0900

    .

--- db-service.js
+++ db-service.js
@@ -7,22 +7,34 @@ async function selectUsers() {
 }
 
 /***
- * CHECK_STOCK
+ * 재고확인
  */
 async function check_stock(branch, item_name) {
     let ret = await dbPool.query("select amount, depot from stock where item_name = ? and branch = ?", [item_name, branch]);
-    //업데이트된 행이 있다면 인증된 것임.
     return ret[0];
 }
 
-async function updateBotUserKey(pincode, bot_user_key) {
-    let ret = await dbPool.query("update users set bot_user_key = ? where pincode = ?", [bot_user_key, pincode]);
-    //업데이트된 행이 있다면 인증된 것임.
-    return ret.affectedRows > 0;
+/***
+ * 납품일정
+ */
+async function supply_schedule(branch, item_name) {
+    let ret = await dbPool.query("select amount, dodate from supply where item_name = ? and branch = ?", [item_name, branch]);
+    return ret[0];
+}
+
+
+/***
+ * 재고부족
+ */
+async function short_stock(branch) {
+    let ret = await dbPool.query('select item_name from stock where amount < 1 and branch = ?', [branch]);
+    return ret;
 }
 
 module.exports = {
     check_stock: check_stock,
+    supply_schedule: supply_schedule,
+    short_stock: short_stock,
     poolShutdown: dbPool.poolShutdown,
 };
 
--- web.js
+++ web.js
@@ -5,7 +5,30 @@ const dbService = require('./db-service');
 const C = require('./contant');
 const Josa = require('josa-js');
 const utils = require('./utils');
-const branchMap = {};
+
+const quickReplies = [
+    {
+        label: '재고확인',
+        action: 'message',
+        messageText: '재고확인'
+    },
+    {
+        label: '납품일정',
+        action: 'message',
+        messageText: '납품일정'
+    },
+    {
+        label: '재고부족',
+        action: 'message',
+        messageText: '재고부족'
+    }
+    ,
+    {
+        label: '지점선택',
+        action: 'message',
+        messageText: '지점선택'
+    }
+];
 
 (async function() {
     app.use(express.json());
@@ -20,9 +43,9 @@ const branchMap = {};
 
     app.post('/select_branch', function(req, res) {
         let body = req.body;
-        let bot_user_key = body.userRequest.user.id;
-        let branch = body.userRequest.utterance;
-        branchMap[bot_user_key] = branch;
+        // let bot_user_key = body.userRequest.user.id;
+        let branch = body.action.params.branch;
+        // branchMap[bot_user_key] = branch;
 
         const responseBody = {
             "version": "2.0",
@@ -34,23 +57,7 @@ const branchMap = {};
                         }
                     }
                 ],
-                quickReplies: [
-                    {
-                        label: '재고확인',
-                        action: 'block',
-                        blockId: '5dbdab1292690d0001e87a26'
-                    },
-                    {
-                        label: '납품일정',
-                        action: 'message',
-                        messageText: '납품일정'
-                    },
-                    {
-                        label: '재고부족',
-                        action: 'message',
-                        messageText: '재고부족'
-                    }
-                ]
+                quickReplies: quickReplies
             }
         };
         res.status(200).send(responseBody);
@@ -62,29 +69,25 @@ const branchMap = {};
     app.post('/check_stock', async function(req, res) {
         let body = req.body;
         let bot_user_key = body.userRequest.user.id;
-        let branch = branchMap[bot_user_key];
+        // let branch = branchMap[bot_user_key];
         // let context = utils.getContext(req.body.contexts, 'global');
+        let branch = req.body.action.params.branch;
         let item_name = req.body.action.params.item;
         let item_with_josa = Josa.r(item_name,'은/는');
         let ret = await dbService.check_stock(branch, item_name);
-        //TODO branch 나 품목이 없을 경우  undefined...처리..
-        if (ret == undefined) {
-            const responseBody = {
-                "version": "2.0",
-                "template": {
-                    "outputs": [
-                        {
-                            "simpleText": {
-                                "text": `에러발생.`
-                            }
-                        }
-                    ]
-                }
-            };
-            res.status(200).send(responseBody);
+        let messageText;
+        if (ret) {
+            let amount = ret.amount;
+            let depot = ret.depot;
+            if (amount > 0) {
+                messageText = `${branch}에 ${item_with_josa} ${amount}개의 재고가 ${depot}창고에 있습니다.`
+            } else {
+                messageText = `${branch}에 ${item_with_josa} 재고가 없습니다.`
+            }
+
+        } else {
+            messageText = `${branch}에는 '${item_name}' 품목이 존재하지 않습니다.`;
         }
-        let amount = ret.amount;
-        let depot = ret.depot;
 
         const responseBody = {
             "version": "2.0",
@@ -92,10 +95,11 @@ const branchMap = {};
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": `${item_with_josa} ${amount}개의 재고가 ${depot}창고에 있습니다.`
+                            "text": messageText
                         }
                     }
-                ]
+                ],
+                quickReplies: quickReplies
             }
         };
         res.status(200).send(responseBody);
@@ -104,23 +108,35 @@ const branchMap = {};
     /**
      * 납품일정
      * */
-    app.post('/supply_schedule', function(req, res) {
-        let dodate = '';
-        let amount = 0;
-        let item_with_josa = '';
+    app.post('/supply_schedule', async function(req, res) {
+        let branch = req.body.action.params.branch;
+        let item_name = req.body.action.params.item;
+        let item_with_josa = Josa.r(item_name,'은/는');
+        let ret = await dbService.supply_schedule(branch, item_name);
+        let messageText;
+        if (ret) {
+            let amount = ret.amount;
+            let dodate = ret.dodate;
+            if (amount > 0) {
+                messageText = `${dodate}에 ${amount}ea 예정입니다.`
+            } else {
+                messageText = `${branch}에 ${item_with_josa} 현재 납품일정이 없습니다.`
+            }
+        } else {
+            messageText = `${branch}에는 '${item_name}' 품목이 존재하지 않습니다.`;
+        }
 
-        let message = `${dodate}에 ${amount}ea 예정입니다.`
-        let message2 = `${item_with_josa} 현재 납품일정이 없습니다.`
         const responseBody = {
             "version": "2.0",
             "template": {
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": message2
+                            "text": messageText
                         }
                     }
-                ]
+                ],
+                quickReplies: quickReplies
             }
         };
         res.status(200).send(responseBody);
@@ -129,9 +145,18 @@ const branchMap = {};
     /**
      * 재고부족
      * */
-    app.post('/short_stock', function(req, res) {
-        let sql = 'select * from stock where amount = 0'
-        let message = `${yyyymmdd}에 ${amount}ea 예정입니다.`;
+    app.post('/short_stock', async function(req, res) {
+        let branch = req.body.action.params.branch;
+        let ret = await dbService.short_stock(branch);
+        let messageText;
+        if (ret && ret.length > 0) {
+            messageText = '';
+            for(let i = 0;i < ret.length; i++) {
+                messageText += ret[i].item_name + ' 재고가 없습니다.\n';
+            }
+        } else {
+            messageText = `${branch}에는 모든 품목에 재고가 있습니다.`;
+        }
 
         const responseBody = {
             "version": "2.0",
@@ -139,10 +164,11 @@ const branchMap = {};
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": message
+                            "text": messageText
                         }
                     }
-                ]
+                ],
+                quickReplies: quickReplies
             }
         };
         res.status(200).send(responseBody);
